6b1cfb09eeebe60fa4398556575a1a926024afc6
chenbilang <15016271589@163.com>
2019-04-08 07:59:23 +0800

    第二次提交

--- aa_parent/aa_biz/src/main/java/com/cbl/aa/exception/ErpException.java
+++
@@ -1,8 +0,0 @@
-package com.cbl.aa.exception;
-
-public class ErpException extends RuntimeException {
-
-	public ErpException(String message){
-		super(message);
-	}
-}
--- aa_parent/aa_client/src/main/java/com/redsum/bos/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.bos.redsum.com/")
-package com.redsum.bos.ws;
